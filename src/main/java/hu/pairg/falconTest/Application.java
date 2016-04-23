be481a4bcca27f14220214ecbc9fd23ac8d05efc
Gergő Pásztor <gergo@joszaki.hu>
2016-04-23 12:07:31 +0200

    create default spring project with gradle

---
+++ src/main/java/hu/pairg/falconTest/Application.java
@@ -0,0 +1,16 @@
+package hu.pairg.falconTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pairg on 2016.04.21..
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
